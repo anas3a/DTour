656f891165f194c7cb36456ac37f68c860f4a42c
Anas Ansary <meanasansary@gmail.com>
2022-09-13 00:38:00 +0600

    signup page complete

---
+++ lib/Screens/signup_screen.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import '../Models/user.dart';
+import '../Models/data_provider.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  User user = User();
+  DataProvider dataProvider = DataProvider();
+
+  @override
+  void initState() {
+    user.name = "";
+    user.password = "";
+    user.email = "";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+            icon: const Icon(Icons.travel_explore), onPressed: () {}),
+        title: const Text('DTour'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+            maxWidth: MediaQuery.of(context).size.width,
+          ),
+          decoration: const BoxDecoration(color: Colors.deepPurple),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Sign up",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Create an account, it's free",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(36),
+                      topRight: Radius.circular(36),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextField(
+                          decoration: const InputDecoration(
+                            labelText: 'Userame',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(
+                              Icons.person,
+                            ),
+                          ),
+                          onChanged: (value) {
+                            user.name = value.toString();
+                          },
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        TextField(
+                            decoration: const InputDecoration(
+                              labelText: 'E-mail',
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(
+                                Icons.mail_outline,
+                              ),
+                            ),
+                            onChanged: (value) {
+                              user.email = value.toString();
+                            }),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        TextField(
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(
+                                Icons.password,
+                              ),
+                            ),
+                            onChanged: (value) {
+                              user.password = value.toString();
+                            }),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 60,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              var userLoginData =
+                                  await dataProvider.getUserByEmail(user.email);
+                              bool emailValid = RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(user.email!);
+                              if (user.name == "" ||
+                                  user.email == "" ||
+                                  user.password == "" ||
+                                  userLoginData.isEmpty == false ||
+                                  emailValid == false) {
+                                var snackBar = SnackBar(
+                                  elevation: 0,
+                                  behavior: SnackBarBehavior.floating,
+                                  backgroundColor: Colors.transparent,
+                                  content: AwesomeSnackbarContent(
+                                    title: 'Wrong Information!',
+                                    message:
+                                        'Not valid or Email already in use',
+                                    contentType: ContentType.failure,
+                                  ),
+                                );
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              } else {
+                                dataProvider.addUser(user);
+                                Navigator.pop(context);
+                                Navigator.pushNamed(context, '/home_page',
+                                    arguments: {'userEmail': user.email});
+                              }
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                            ),
+                            child: const Text('Sign up'),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Already have an account? "),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                                Navigator.pushNamed(context, '/');
+                              },
+                              child: const Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
