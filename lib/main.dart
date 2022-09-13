140c8d5c07a10b09220fef64b9b3307b0b66f40b
Anas Ansary <meanasansary@gmail.com>
2022-09-14 00:44:32 +0600

    add route in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Screens/login_screen.dart';
 import 'Screens/signup_screen.dart';
+import 'Screens/home_screen.dart';
+import 'Screens/spot_details_screen.dart';
+import 'Screens/tour_input_screen.dart';
+import 'Screens/cost_details_screen.dart';
+import 'Screens/tour_purchase_history.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +25,11 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const LoginPage(),
         '/signUp_page': (context) => const SignupPage(),
+        '/home_page': (context) => const HomePage(),
+        '/spot_details_page': (context) => const SpotDetailsPage(),
+        '/tour_input_page': (context) => const TourInputPage(),
+        '/cost_details_page': (context) => const CostDetailsPage(),
+        '/purchase_history_page': (context) => const TourPurchaseHistory(),
       },
       debugShowCheckedModeBanner: false,
     );
